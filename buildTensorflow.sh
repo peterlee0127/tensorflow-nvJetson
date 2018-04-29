acfc4dc3382a57f3ab912f042ec4b0ca9d8f0d4e
Peter Lee <peterlee0127@gmail.com>
2018-04-29 22:19:56 +0800

    update patch for tensorflow 1.7

--- buildTensorflow.sh
+++ buildTensorflow.sh
@@ -3,10 +3,10 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/extras/CUPTI/lib64
 sudo apt-get install python-numpy python-dev python-pip python-wheel
 
 git clone https://github.com/tensorflow/tensorflow.git
-cp tensorflow1.7.patch tensorflow
+cp tensorflow1.8.patch tensorflow
 cd tensorflow
-git checkout v1.7.0
-git apply tensorflow1.7.patch
+git checkout v1.8.0
+git apply tensorflow1.8.patch
 ./configure
 
 bazel build --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
