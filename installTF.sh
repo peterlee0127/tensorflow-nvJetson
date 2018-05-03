a50caf2fcffc563f922707238aca24f7d21934f2
Peter Lee <peterlee0127@gmail.com>
2018-05-03 10:30:42 +0800

    add installTF.sh
    Former-commit-id: d19a8d869cb3fc21a303cc3122db000e093710d2

---
+++ installTF.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+FILE=tensorflow-1.8.0-cp27-cp27mu-linux_aarch64.whl
+wget https://tfjetson.peterlee0127.com/$FILE-part-1
+wget https://tfjetson.peterlee0127.com/$FILE-part-2
+
+
+cat $FILE-part-* > $FILE
