9819db07a717e239f575a0180048fa736118a200
Peter Lee <peterlee0127@gmail.com>
2018-05-06 14:23:37 +0800

    add install hint

--- installTF.sh
+++ installTF.sh
@@ -3,5 +3,6 @@ FILE=tensorflow-1.8.0-cp27-cp27mu-linux_aarch64.whl
 wget https://tfjetson.peterlee0127.com/$FILE-part-1
 wget https://tfjetson.peterlee0127.com/$FILE-part-2
 
-
 cat $FILE-part-* > $FILE
+
+echo "you can install by 'sudo pip install $FILE'"
