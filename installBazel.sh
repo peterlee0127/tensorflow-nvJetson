54faa5a74f3b2841aea4d1a3710d623f2a7c9eb1
Peter Lee <peterlee0127@gmail.com>
2018-07-31 15:02:25 +0800

    update bazel

--- installBazel.sh
+++ installBazel.sh
@@ -1,15 +1,15 @@
 #! /bin/sh
 sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update && sudo apt-get install  build-essential python zip oracle-java8-installer
+sudo apt-get update
+sudo apt-get install  build-essential python zip oracle-java8-installer
 
 mkdir bazel
 cd bazel
 
-bazel_version="0.11.1"
+bazel_version="0.15.2"
 wget https://github.com/bazelbuild/bazel/releases/download/$bazel_version/bazel-$bazel_version-dist.zip
 
 unzip bazel-$bazel_version-dist.zip
 ./compile.sh
 
 sudo cp output/bazel /usr/local/bin
-
